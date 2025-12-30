ab81fd5a484242f517c498d35005e4f41fe0a15c
shinyangdevelop <shinyangdevelop@gmail.com>
2025-12-30 20:21:26 +0900

    Install nodemon

--- communication/index.js
+++ communication/index.js
@@ -3,7 +3,7 @@ const { nanoid } = require("nanoid");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = 3389;
 
 // In-memory store for data
 const dataStore = {};
